9ad53cee05c6b7d2dd3dc25eb6ae9d0d5e952296
XavierEyo <udoeyo91@gmail.com>
2022-10-10 05:02:42 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
 	if (!array || !size)
 		return;
 
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		min = i;
 		for(j = i + 1; j <= size; j++)
@@ -23,7 +23,7 @@ void selection_sort(int *array, size_t size)
 			{
 				min = j;
 				temp = array[min];
-				array[min] = array [i];
+				array[min] = array[i];
 				array[i] = temp;
 				print_array(array, size);
 			}
